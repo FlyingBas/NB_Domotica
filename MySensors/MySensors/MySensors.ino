69fb4e7ada6b6adc82482a0771be758874158ddd
Bastiaan Wesselink <sawesselink@hotmail.com>
2018-01-04 15:49:13 +0100

    Added initial project for MySensors setup

---
+++ MySensors/MySensors/MySensors.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MySensors.ino
+ Created:	04-Jan-18 15:37:07
+ Author:	Bastiaan
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
